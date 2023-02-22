b6fa8ea699e6c75f872a1b53c0f194d8ab740cc2
Julie C <jucheng925@gmail.com>
2023-02-22 13:34:21 -0500

    DOM modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+//document.body.append(newHeader);//
+
+//newHeader.setAttribute("id", "victory");//
+newHeader.id = "victory"
+
+const message = document.getElementById("victory");
+message.textContent = 'Kimchi is the champion'
