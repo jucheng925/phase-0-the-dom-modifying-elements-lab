05d755023ddd178dbae72f69acfd7370567c6514
Julie C <jucheng925@gmail.com>
2023-04-14 11:11:27 -0400

    change in coding after better understanding of dom

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const element = document.getElementById("main");
 element.remove();
 
 const newHeader = document.createElement("h1");
-document.body.appendChild(newHeader);
+//document.body.appendChild(newHeader);
 //document.body.append(newHeader);//
 
 //newHeader.setAttribute("id", "victory");//
 newHeader.id = "victory"
 
-const message = document.getElementById("victory");
-message.textContent = 'Kimchi is the champion'
+//const message = document.getElementById("victory");
+newHeader.textContent = 'Kimchi is the champion'
+document.body.append(newHeader);
